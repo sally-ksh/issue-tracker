800d0ff9398c12721870f3c1a457776d79cc07cb
jthw1005 <skl0105@naver.com>
2022-06-16 18:19:50 +0900

    [juolly/issue-tracker #7] design: issue filter 구현 중

--- FE/src/pages/IssueList/index.tsx
+++ FE/src/pages/IssueList/index.tsx
@@ -1,5 +1,18 @@
+import { ReactComponent as ArrowDown } from "@/assets/ArrowDown.svg";
+import * as S from "@/pages/IssueList/style";
+
 const IssueList = () => {
-  return <div>IssueList</div>;
+  return (
+    <>
+      <S.FilterContainer>
+        <S.FilterOptionButton>
+          필터 <ArrowDown />
+        </S.FilterOptionButton>
+        <S.FilterInput defaultValue="필터 인풋" />
+      </S.FilterContainer>
+      <S.FilterOptionPopUp />
+    </>
+  );
 };
 
 export default IssueList;
