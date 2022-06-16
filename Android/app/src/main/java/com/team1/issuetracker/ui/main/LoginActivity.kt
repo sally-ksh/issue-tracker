22bb2eca3b4abaffb62183f127daedbe2270f2f9
ese111 <feung94@gmail.com>
2022-06-16 12:22:47 +0900

    feat: 레이아웃 구성 중

--- Android/app/src/main/java/com/team1/issuetracker/ui/main/LoginActivity.kt
+++ Android/app/src/main/java/com/team1/issuetracker/ui/main/LoginActivity.kt
@@ -1,20 +1,25 @@
 package com.team1.issuetracker.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.team1.issuetracker.R
 import com.team1.issuetracker.databinding.ActivityLoginBinding
-import com.team1.issuetracker.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
+    }
+
+    private fun setTempMove() {
+        binding.btnGithubLogin.setOnClickListener {
+
+        }
     }
 }
