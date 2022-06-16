0ad8e2187de6f83fd8f35d827ecc6f10833a5f18
hanchang97 <nimok97@gmail.com>
2022-06-16 15:08:53 +0900

    feat : jetpack navigation 연결 BottomNavigatioinView 각 프래그먼트와 연결

--- Android/app/src/main/java/com/team1/issuetracker/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/team1/issuetracker/ui/login/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.team1.issuetracker.ui.main
+package com.team1.issuetracker.ui.login
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.team1.issuetracker.R
 import com.team1.issuetracker.databinding.ActivityLoginBinding
+import com.team1.issuetracker.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
