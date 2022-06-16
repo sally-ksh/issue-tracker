26d63fa1db0dee6209befeba687284319947bcd6
hanchang97 <nimok97@gmail.com>
2022-06-17 01:09:41 +0900

    feat : issue 화면 구성 상단바, fab 배치

--- Android/app/src/main/java/com/team1/issuetracker/ui/main/issue/IssueFragment.kt
+++ Android/app/src/main/java/com/team1/issuetracker/ui/main/issue/IssueFragment.kt
@@ -1,12 +1,15 @@
 package com.team1.issuetracker.ui.main.issue
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.team1.issuetracker.R
+import com.team1.issuetracker.common.PrintLog
 import com.team1.issuetracker.databinding.FragmentIssueBinding
 
 class IssueFragment: Fragment() {
@@ -27,6 +30,22 @@ class IssueFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setAppBar()
+    }
 
+    private fun setAppBar(){
+        binding.topAppBar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.issue_search -> {
+                    PrintLog.printLog("issue search")
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            PrintLog.printLog("issue filter")
+        }
     }
 }
