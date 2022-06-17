6c4130b1fc85346e6ed45f5b0a72e488ac8d41f6
jwu-ice <sju02048@naver.com>
2022-06-17 10:36:48 +0900

    [juolly/issue-tracker #17] design: InputBox 추가

---
+++ FE/src/components/common/InputBox/style.ts
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+import { InputBoxProps } from "@/components/common/InputBox/index";
+import { COLOR, FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
+
+export const InputBox = styled.input<InputBoxProps>`
+  ::placeholder {
+    color: ${COLOR["gray-700"]};
+  }
+
+  ${({ fontSize }) =>
+    fontSize &&
+    css`
+      font-size: ${FONT_SIZE[fontSize]};
+    `}
+
+  ${({ fontWeight }) =>
+    fontWeight &&
+    css`
+      font-weight: ${FONT_WEIGHT[fontWeight]};
+    `}
+
+    ${({ color }) =>
+    color &&
+    css`
+      color: ${COLOR[color]};
+    `}
+`;
