b2a4ea4dd88cc107989dc5115eef01c5e6f9a2cf
sally-ksh <sally.devz@gmail.com>
2022-06-22 08:07:02 +0900

    [juolly/issue-tracker #37] feat : IssueLabel

---
+++ BE/src/main/java/com/sh/issuetracker/issuelabel/IssueLabel.java
@@ -0,0 +1,37 @@
+package com.sh.issuetracker.issuelabel;
+
+import com.sh.issuetracker.issue.Issue;
+import com.sh.issuetracker.label.Label;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.ToString;
+
+@ToString(exclude = "issue")
+@Table(name = "label_has_issue")
+@Entity
+public class IssueLabel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "issue_label")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "issue_id")
+	private Issue issue;
+
+	@ManyToOne
+	@JoinColumn(name = "label_id")
+	private Label label;
+
+	public String labelName() {
+		return this.label.getName();
+	}
+}
