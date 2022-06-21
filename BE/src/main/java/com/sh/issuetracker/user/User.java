5c5fc07ccfb908dbcfff572f27a316ef74644286
sally-ksh <sally.devz@gmail.com>
2022-06-22 08:07:42 +0900

    [juolly/issue-tracker #37] feat : User

---
+++ BE/src/main/java/com/sh/issuetracker/user/User.java
@@ -0,0 +1,33 @@
+package com.sh.issuetracker.user;
+
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(of = "id")
+@Where(clause = "is_deleted = false")
+@Table(name = "issue_tracker_user")
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	private String nickName;
+	private String email;
+	private String password;
+	private String image;
+	private boolean isDeleted;
+
+	public String nickName() {
+		return nickName;
+	}
+}
