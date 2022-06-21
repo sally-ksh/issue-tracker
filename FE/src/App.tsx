a39baef94f6786eaa18a6a9a9af98aefca1d5faa
jwu-ice <sju02048@naver.com>
2022-06-21 14:28:50 +0900

    [juolly/issue-tracker #16] feat: react-query 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@/pages/Home";
@@ -12,21 +14,25 @@ import NotFound from "@/pages/NotFound";
 const App = () => {
   // FIXME 임시 OAuth
   const isOAuth = true;
+  const queryClient = new QueryClient();
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={isOAuth ? <Home /> : <Login />}>
-          <Route index element={<IssueList />} />
-          <Route path="/issue-detail" element={<IssueDetail />} />
-          <Route path="/issue-register" element={<IssueRegister />} />
-          <Route path="/label" element={<Label />} />
-          <Route path="/milestone" element={<Milestone />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={isOAuth ? <Home /> : <Login />}>
+            <Route index element={<IssueList />} />
+            <Route path="/issue-detail" element={<IssueDetail />} />
+            <Route path="/issue-register" element={<IssueRegister />} />
+            <Route path="/label" element={<Label />} />
+            <Route path="/milestone" element={<Milestone />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
