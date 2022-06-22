94f495792c9c2a3e9b9743e05193a7d913a50ef4
ese111 <feung94@gmail.com>
2022-06-23 00:29:52 +0900

    fea: layout 구성 완료

---
+++ Android/app/src/main/java/com/team1/issuetracker/data/model/MileStone.kt
@@ -0,0 +1,14 @@
+package com.team1.issuetracker.data.model
+
+data class MileStone(
+    val id: Int,
+    val title: String,
+    val progress : Int,
+    val content: String,
+    val date: String,
+    val open: Int,
+    val closed: Int,
+    var isSwiped: Boolean = false,
+    var isCheckVisible: Boolean = false,
+    var isChecked: Boolean = false
+)
