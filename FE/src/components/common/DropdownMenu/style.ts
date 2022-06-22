56e8f0903fb7225188756bebf53e8a5a3a0cf336
jthw1005 <skl0105@naver.com>
2022-06-22 10:29:05 +0900

    [juolly/issue-tracker #33] design: Dropdown menu UI

---
+++ FE/src/components/common/DropdownMenu/style.ts
@@ -0,0 +1,27 @@
+import styled, { css } from "styled-components";
+
+import { FONTSIZE } from "@/styles/constTheme";
+
+type DropdownMenuBoxProps = {
+  isVisible?: boolean;
+  isBtnOnLeftSide?: boolean;
+};
+
+export const DropdownMenuBox = styled.div`
+  position: relative;
+`;
+
+export const DropdownMenuBtn = styled.button`
+  font-size: ${FONTSIZE.L}rem;
+`;
+
+export const DropdownMenuListBox = styled.div<DropdownMenuBoxProps>`
+  position: absolute;
+  top: 20rem;
+  left: ${(props) => (props.isBtnOnLeftSide ? "0rem" : "null")};
+  right: ${(props) => (props.isBtnOnLeftSide ? "null" : "0rem")};
+  max-height: ${(props) => (props.isVisible ? "500rem" : "0px")};
+  overflow: hidden;
+  width: 240rem;
+  z-index: 1;
+`;
