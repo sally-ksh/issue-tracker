8e05b1956220c46b102d7df772e81faa66bdac52
sally-ksh <sally.devz@gmail.com>
2022-06-24 13:22:47 +0900

    [juolly/issue-tracker #43]  feat : IssueSearchRequest

---
+++ BE/src/main/java/com/sh/issuetracker/issue/dto/IssueSearchRequest.java
@@ -0,0 +1,19 @@
+package com.sh.issuetracker.issue.dto;
+
+import org.apache.logging.log4j.util.Strings;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class IssueSearchRequest {
+	private String q;
+
+	public String getText() {
+		if (Strings.isBlank(q)) {
+			return "is:open";
+		}
+		return q;
+	}
+}
