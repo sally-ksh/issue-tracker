3370ea60ee8fd425dd08f1b4fde89b82a37731d8
sally-ksh <sally.devz@gmail.com>
2022-07-01 08:37:04 +0900

    [juolly/issue-tracker#68] feat : OauthTokenParam

---
+++ BE/src/main/java/com/sh/issuetracker/login/oauth/dto/OauthTokenParam.java
@@ -0,0 +1,17 @@
+package com.sh.issuetracker.login.oauth.dto;
+
+public class OauthTokenParam {
+	public static final String GRANT_TYPE = "grant_type";
+	public static final String VALUE_OF_GRANT_TYPE = "authorization_code";
+	public static final String CODE = "code";
+	public static final String REDIRECT_URI = "redirect_uri";
+	public static final String CLIENT_ID = "client_id";
+	public static final String CLIENT_SECRET = "client_secret";
+	public static final String STATE = "state";
+	public static final String SCOPE = "scope";
+	public static final String ACCESS_TOKEN = "access_token";
+
+	public static final String AUTHORIZATION = "Authorization";
+	public static final String GOOGLE_VALUE_OF_AUTHORIZATION = "Bearer oauth2-token";
+	public static final String GITHUB_VALUE_OF_AUTHORIZATION = "Bearer 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";
+}
